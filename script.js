ad4b004fec0f255d5a417330d5e76b54a5c9713c
glitter545 <bobbybroken992@gmail.com>
2023-01-28 03:41:12 -0500

    Created grid

---
+++ script.js
@@ -0,0 +1,13 @@
+document.body.style.backgroundColor = "antiquewhite";
+document.body.style.height = "100vh";
+document.body.style.display = "flex";
+document.body.style.justifyContent = "center";
+document.body.style.alignItems = "center";
+
+const grid = document.getElementById("grid");
+grid.style.width = "500px";
+grid.style.height = "500px";
+grid.style.border = "2px solid blue";
+
+
+
